6660027278b8a479618f4a7e4b95ee0617137218
Matt Linder <mjlinder218@gmail.com>
2021-04-15 13:07:07 -0700

    Added objects for questions and improved countdown and askQuestions functions

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,27 +1,50 @@
+// Place in doc for timer
 var timerEl = document.getElementById('countdown');
+// Place in doc for text [???????]
 var mainEl = document.getElementById('main');
-var questions = ["a", "b", "c", "d"];
+
+// objects and variables for quiz questions
+var q1 = {
+    q: "How beautiful is Jane?", 
+    answers: ["All of these", "Exceedingly", "Overwhelmingly", "Inordinately"],
+    correct: 1
+};
+var q2 = {
+    q: "How Hungry are the cats?", 
+    answers: ["All of these", "Exceedingly", "Overwhelmingly", "Inordinately"],
+    correct: 1
+};
+var q3 = {
+    q: "How great is Mauser?", 
+    answers: ["All of these", "Exceedingly", "Overwhelmingly", "Inordinately"],
+    correct: 1
+};
+var questions = [q1, q2, q3];
 var questionIndex = 0;
 
-function askQuestions() {
-    for (let index = 0; index < questions.length; index++) {
-        const element = array[index];
-        
+// Function to ask questions
+function askQuestion() {
+    mainEl.textContent = questions[questionIndex].q;
+    console.log(questions[questionIndex].answers[0]);
+    for (let i = 0; i < 4; i++) {
+        var btn = document.createElement("button");   
+        btn.innerHTML = questions[questionIndex].answers[i];                   
+        document.body.appendChild(btn);  
     }
-    // Original version:
-    // mainEl.textContent=questions[questionIndex];
-    // questionIndex++;
+    questionIndex++;
+
+    // On button click, present next question and result of last q.
+
 }
 
-// Adapted from 10-Stu_timers-Intervals
+// Timer function (adapted from 10-Stu_timers-Intervals activity)
 function countdown() {
   var timeLeft = 3;
-
+  timerEl.textContent = timeLeft;
   var timeInterval = setInterval(function () {
     if (timeLeft >= 1) {
-      timerEl.textContent = timeLeft + ' blah--';
+      timerEl.textContent = timeLeft;
       timeLeft--;
-      askQuestions();
     } else {
       timerEl.textContent = '';
       clearInterval(timeInterval);
@@ -35,4 +58,5 @@ function quizOver() {
     window.location.href = './quizover.html';
 }
 
+askQuestion();
 countdown();
