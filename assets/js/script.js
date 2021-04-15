e908d691f30173f83434b606ac95315f8f2b2c6a
Matt Linder <mjlinder218@gmail.com>
2021-04-15 10:34:52 -0700

    Skeletal starting point

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var startbuttonEl = document.querySelector("#start");
+
+// Attach event listener to start button element
+startbuttonEl.addEventListener("click", function() {
+    window.location.href = './quiz.html';
+  });
+  
