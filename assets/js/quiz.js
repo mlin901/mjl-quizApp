01bd47f23ba3dff5c67de077e0e86d0c63739620
Matt Linder <mjlinder218@gmail.com>
2021-04-16 07:46:01 -0700

    Fixed issue that prevented last question from being answered.

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,11 +1,9 @@
 var timeLeft = 30;
-// Place in doc for timer
-var timerEl = document.getElementById('countdown');
-// Place in doc for text [???????]
-var mainEl = document.getElementById('main');
-var feedbackEl = document.getElementById('feedback');
+var timerEl = document.getElementById('countdown'); // Timer location
+var mainEl = document.getElementById('main'); // Question text location
+var feedbackEl = document.getElementById('feedback'); // Location for "correct" or "wrong"
 
-// objects and variables for quiz questions
+// Objects for questions and possible answers
 var q1 = {
     q: "First question", 
     answers: ["correct", "Exceedingly", "Overwhelmingly", "Inordinately"],
@@ -26,6 +24,8 @@ var q4 = {
     answers: ["alpha", "Beta", "gamma", "correct"],
     correct: "button3"
 };
+// Array and index for questions  
+// ****Could I just create an array of objects?????*******
 var questions = [q1, q2, q3, q4];
 var questionIndex = 0;
 
@@ -38,23 +38,22 @@ function askQuestion() {
         // Add buttons for mult-choice questions to page
         for (let i = 0; i < questions[questionIndex].answers.length; i++) {
             var ifBtn = document.getElementById("button"+i);
-            // If button exists...
+            // If button already exists...
             if (ifBtn !== null) {
-                ifBtn.innerHTML = questions[questionIndex].answers[i];   //***                 
+                ifBtn.innerHTML = questions[questionIndex].answers[i];                 
             } else {  //If button doesn't yet exist...
                 var ifBtn = document.createElement("button");  
                 ifBtn.id = "button" + i;
-                ifBtn.innerHTML = questions[questionIndex].answers[i];  // *****                 
-                document.body.appendChild(ifBtn);  // *****
+                ifBtn.innerHTML = questions[questionIndex].answers[i];                 
+                document.body.appendChild(ifBtn); 
             }
         }
-        
     } else {  // If there are no more questions...
         setTimeout(function(){ quizOver(); }, 30000);
-        // quizOver(); // ****really? 
     }
+    // Move to next question by incrementing index for question array
     questionIndex++;
-    // return
+    // return   // *****???????
 }
 
 // Timer function (adapted from 10-Stu_timers-Intervals activity)
@@ -72,33 +71,31 @@ function countdown() {
   }, 1000);
 }
 
-// Determine if user clicked the correct answer?
+// Determine if user clicked the correct answer
 function evaluateAnswer(clickedBtn) {
     // If answer is correct...
     if (clickedBtn == questions[questionIndex-1].correct) {
         console.log("Oh, heck yeah");
-        // ***** Display a "correct" message
         feedbackEl.textContent = "Correct!";
         // increment counter for correct answers ******
-
     } else { // If answer is wrong...
-        console.log("Nooooo");
-        // Display an "incorrect message"
-        feedbackEl.textContent = "Wrong!";
+        feedbackEl.textContent = "Answer is incorrect.";
         // Remove a few seconds from timer for wrong answer
         timeLeft = timeLeft - 2;
     }
     askQuestion();  
 }
 
-// On button click, present next question and result of last q.
-// Adapted from https://davidwalsh.name/event-delegate
+// On button click, call function to evaluate answer (clicked button)
+// (Adapted from https://davidwalsh.name/event-delegate)
 document.addEventListener("click",function(event) {
     // event.target was the clicked element
+    // If the click is on a button...
     if (event.target && event.target.matches("button")) {
-        if (questionIndex < questions.length) { 
-        evaluateAnswer(event.target.id);  
-        } else {
+        // If we haven't already evaluated the last question...
+        if (questionIndex <= questions.length) { 
+            evaluateAnswer(event.target.id);  
+        } else { // If all questions have been answered...
             setTimeout(function(){ quizOver(); }, 1000); // SO WHAT HAPPENS WITH THE OTHER ONE?
         }
     }
