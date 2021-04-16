94ba61390f445ed97912a8319b95ac5d17b8d072
Matt Linder <mjlinder218@gmail.com>
2021-04-16 09:50:27 -0700

    Changed locally stored data to an object

--- assets/js/quizover.js
+++ assets/js/quizover.js
@@ -1,7 +1,6 @@
 var scoreEl = document.getElementById('score');
 var mainTextEl = document.getElementById('mainText');
 var doneButton = document.querySelector("#done");
-// var msgDiv = document.querySelector("#msg");
 
 var score = localStorage.getItem("CurrentQuiz");
 
@@ -11,30 +10,41 @@ console.log(score);
 scoreEl.textContent = "Your score: " + score;
 mainTextEl.textContent = "Please enter you initials and click Done.";
 
-// displayMessage functon and event listener adapted from 
-// 22-Stu_Local-storage activity
-
-// function displayMessage(type, message) {
-//     msgDiv.textContent = message;
-//     msgDiv.setAttribute("class", type);
-// }
+// Event listener adapted from 22-Stu_Local-Storage and 
+// 24-Stu_Local_Storage_Objects activities
 
 doneButton.addEventListener("click", function(event) {
     event.preventDefault(); // ****Add same to main quiz script event listener?
-  
     var initials = document.querySelector("#initials").value;
-  
     if (initials === "") {
         alert("Error: Initials field cannot be blank");
-        // displayMessage("error", "Initials field cannot be blank");
     } else {
-        localStorage.setItem(initials, score);
-        alert("Score recorded");
-        // displayMessage("success", "Score recorded");
-        window.location.href = './scores.html';
-    //   renderLastRegistered();
-    }
-    
+        // create user object from submission
+        var scoreObj = {
+            initials: initials,
+            score: score
+        };
+        // store to local storage 
+        localStorage.setItem("codeQuiz_"+initials, JSON.stringify(scoreObj));
 
-  });
+        alert("Score recorded");
+        window.location.href = './scores.html';
+    }
+});
+
+//  // *******
+ 
+//  doneButton.addEventListener("click", function(event) {
+//     event.preventDefault();
+//     var initials = document.querySelector("#initials").value;
+
+//     // create user object from submission
+//     var scoreObj = {
+//       initials: initials.value.trim(),
+//       score: score
+//     };
+//     // store to local storage 
+//     localStorage.setItem("codeQuiz_"+initials, JSON.stringify(scoreObj));
+    
+// });
 
