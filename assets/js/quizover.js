0a11d97c2dca7adfb2dbb0a918bc435441a1532b
Matt Linder <mjlinder218@gmail.com>
2021-04-16 09:17:08 -0700

    Added event listener and displayMessage function

--- assets/js/quizover.js
+++ assets/js/quizover.js
@@ -1,8 +1,37 @@
 var scoreEl = document.getElementById('score');
 var mainTextEl = document.getElementById('mainText');
+var doneButton = document.querySelector("#done");
+var msgDiv = document.querySelector("#msg");
+
 var score = localStorage.getItem("CurrentQuiz");
+
 
 console.log(score);
 
 scoreEl.textContent = "Your score: " + score;
 mainTextEl.textContent = "Please enter you initials and click Done.";
+
+// displayMessage functon and event listener adapted from 
+// 22-Stu_Local-storage activity
+
+function displayMessage(type, message) {
+    msgDiv.textContent = message;
+    msgDiv.setAttribute("class", type);
+  }
+
+doneButton.addEventListener("click", function(event) {
+    event.preventDefault(); // ****Add same to main quiz script event listener?
+  
+    var initials = document.querySelector("#initials").value;
+  
+    if (initials === "") {
+      displayMessage("error", "Initials field cannot be blank");
+    } else {
+      displayMessage("success", "Score recorded");
+      localStorage.setItem(initials, score);
+    //   renderLastRegistered();
+    }
+    window.location.href = './scores.html';
+
+  });
+
