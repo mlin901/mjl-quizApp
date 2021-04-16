889458d041499a37f85ea9fd38e4e2bdf765d758
Matt Linder <mjlinder218@gmail.com>
2021-04-16 08:33:11 -0700

    Added basic text for page and retrieval/display of quiz score from local storage

--- assets/js/quizover.js
+++ assets/js/quizover.js
@@ -1,2 +1,8 @@
+var scoreEl = document.getElementById('score');
 var mainTextEl = document.getElementById('mainText');
-mainTextEl.textContent = "Quiz over. Please enter you initials and press Done.";
+var score = localStorage.getItem("CurrentQuiz");
+
+console.log(score);
+
+scoreEl.textContent = "Your score: " + score;
+mainTextEl.textContent = "Please enter you initials and click Done.";
